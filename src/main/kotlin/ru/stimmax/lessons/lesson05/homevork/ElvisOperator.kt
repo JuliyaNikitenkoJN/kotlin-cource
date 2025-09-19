a8281b0591f0c90d9045a564418bbdc5da0a5a0a
Juliya <testjn2020@gmail.com>
2025-09-19 19:09:28 +0300

    All tasks done

--- src/main/kotlin/ru/stimmax/lessons/lesson05/homevork/ElvisOperator.kt
+++ src/main/kotlin/ru/stimmax/lessons/lesson05/homevork/ElvisOperator.kt
@@ -5,31 +5,16 @@ val soundStartPercent: Double = 100.0
 var coefAttenuationN: Double? = null
 val coefAttenuationStandart: Double = 0.5
 var resultIntensity: Double = soundStartPercent * (coefAttenuationN ?: coefAttenuationStandart)
-        println(resultIntensity) }
+        println(resultIntensity)
 
-Задача 2
-Контекст: Клиент оплачивает доставку груза. К стоимости доставки добавляется
-страховка на груз, которая составляет 0,5% от его стоимости.
-В случае, если стоимость не указана, то берётся стандартная стоимость в $50
-Задача: Рассчитать полную стоимость доставки.
+    var costDelivery: Double? = 30.0
+    var costOfCargo: Double? = null
+    val costOfCargoStandart: Double = 50.0
+    val insurancePercent: Double = 0.5
+    var resultDelivery: Double = (costDelivery ?: 0.0) + (costOfCargo ?: costOfCargoStandart ) / 100 * insurancePercent
+    println(resultDelivery)
 
-un main() {
-    val soundStartPercent: Double = 100.0
-    var coefAttenuationN: Double? = null
-    val coefAttenuationStandart: Double = 0.5
-    var resultIntensity: Double = soundStartPercent * (coefAttenuationN ?: coefAttenuationStandart)
-    println(resultIntensity) }
-
-Задача 3
-Контекст: Вы проводите метеорологические измерения. Одним из важных
-показателей является атмосферное давление, которое должно быть зафиксировано.
-Лаборант приносит вам набор показателей, но по пути может что-нибудь потерять.
-Задача - сообщить об ошибке в случае отсутствия показаний атмосферного давления.
-
-
-un main() {
-    val soundStartPercent: Double = 100.0
-    var coefAttenuationN: Double? = null
-    val coefAttenuationStandart: Double = 0.5
-    var resultIntensity: Double = soundStartPercent * (coefAttenuationN ?: coefAttenuationStandart)
-    println(resultIntensity) }
+    var atmosfericPressure: Double? = null
+    var attentionMessage: String = "Attention, data of atmosferic pressure is lost "
+    var resultAPressure = atmosfericPressure ?: attentionMessage
+    println(resultAPressure) }
