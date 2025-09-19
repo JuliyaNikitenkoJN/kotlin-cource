dda869055cfd9202fa5d0f5b27d321a82ae9748f
Juliya <testjn2020@gmail.com>
2025-09-19 20:36:47 +0300

    All tasks done

--- src/main/kotlin/ru/stimmax/lessons/lesson05/homevork/ElvisOperator.kt
+++ src/main/kotlin/ru/stimmax/lessons/lesson05/homevork/ElvisOperator.kt
@@ -1,20 +1,21 @@
 package ru.stimmax.lessons.lesson05.homevork
 
 fun main() {
-val soundStartPercent: Double = 100.0
-var coefAttenuationN: Double? = null
-val coefAttenuationStandart: Double = 0.5
-var resultIntensity: Double = soundStartPercent * (coefAttenuationN ?: coefAttenuationStandart)
-        println(resultIntensity)
+    val soundStartPercent: Double = 100.0
+    var coefAttenuationN: Double? = null
+    val coefAttenuationStandart: Double = 0.5
+    var resultIntensity: Double = soundStartPercent * (coefAttenuationN ?: coefAttenuationStandart)
+    println(resultIntensity)
 
-    var costDelivery: Double? = 30.0
+    var costDelivery: Double = 30.0
     var costOfCargo: Double? = null
     val costOfCargoStandart: Double = 50.0
     val insurancePercent: Double = 0.5
-    var resultDelivery: Double = (costDelivery ?: 0.0) + (costOfCargo ?: costOfCargoStandart ) / 100 * insurancePercent
+    var resultDelivery: Double = (costDelivery) + (costOfCargo ?: costOfCargoStandart) / 100 * insurancePercent
     println(resultDelivery)
 
     var atmosfericPressure: Double? = null
     var attentionMessage: String = "Attention, data of atmosferic pressure is lost "
     var resultAPressure = atmosfericPressure ?: attentionMessage
-    println(resultAPressure) }
+    println(resultAPressure)
+}
